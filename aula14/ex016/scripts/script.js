1599f9ec1fd77ea07f6e29bca5010293d96020c8
Felipe Souza <129899036+felipesr01@users.noreply.github.com>
2023-08-01 13:41:17 -0300

    Update script.js

--- aula14/ex016/scripts/script.js
+++ aula14/ex016/scripts/script.js
@@ -1,12 +1,31 @@
 function contar() {
-    var inicio = document.querySelector('input#txtinicio')
-    var fim = document.querySelector('input#txtfim')
-    var passo = document.querySelector('input#txtpasso')
+    var inicio = document.querySelector('input#txtinicio').value
+    var fim = document.querySelector('input#txtfim').value
+    var passo = document.querySelector('input#txtpasso').value
+    var res = document.querySelector('div#res')
 
-    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
-        window.alert = 'dados fodade'
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
+        res.innerHTML = 'Dados insuficientes... Preencha os espaços em branco e tente de novo'
     } else {
-        window.alert = 'ok'
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(inicio)
+        let f = Number(fim)
+        let p = Number(passo)
+        if (p <= 0) {
+            window.alert('Passo invalido... Considerando passo 1')
+            p = 1
+        }
+        //Crescente
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}👉 `
+            }
+        //Regressiva
+        } else {
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c}👉 `
+            }
+        }
+        res.innerHTML += `🏁`
     }
-
 }
